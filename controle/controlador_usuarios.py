0a8db8e87118a6be6f6b197eb98d13aad64ae5a4
Luiz Fernando Schelbauer <nandoschelba@gmail.com>
2023-06-19 22:13:04 -0300

    Add persistencia de usuarios e categorias

--- controle/controlador_usuarios.py
+++ controle/controlador_usuarios.py
@@ -1,11 +1,12 @@
 from entidade.usuario_juridico import UsuarioJuridico
 from entidade.usuario_fisico import UsuarioFisico
+from persistencia.usuario_dao import UsuarioDAO
 
 
 class ControladorUsuarios:
     def __init__(self, controlador_sistema):
         from limite.tela_usuario import TelaUsuario
-        self.__usuarios = []
+        self.__usuario_dao = UsuarioDAO()
         self.__controlador_sistema = controlador_sistema
         self.__tela_usuario = TelaUsuario(self)
         self.usuario_logado = None
@@ -17,22 +18,22 @@ class ControladorUsuarios:
         if not isinstance(nome, str) or not isinstance(email, str) or not isinstance(cnpj, int):
             return self.__tela_usuario.mostra_mensagem("\nDados do usuário inválidos. Certifique-se de que o nome é uma"
                                                        " string, o email é uma string e o cnpj são apenas números.")
-        for usuario in self.__usuarios:
+        for usuario in self.__usuario_dao.get_all():
             if isinstance(usuario, UsuarioFisico) and (usuario.email == email or usuario.identificador() == cnpj):
                 return self.__tela_usuario.mostra_mensagem("\nJá existe um usuário cadastrado com este e-mail ou CNPJ.")
         usuario_juridico = UsuarioJuridico(nome, email, cnpj)
-        self.__usuarios.append(usuario_juridico)
+        self.__usuario_dao.add(usuario_juridico.identificador(), usuario_juridico)
         self.__tela_usuario.mostra_mensagem("\nCadastro relizado com sucesso!")
 
     def adiciona_usuario_fisico(self, nome, email, cpf):
         if not isinstance(nome, str) or not isinstance(email, str) or not isinstance(cpf, int):
             return self.__tela_usuario.mostra_mensagem("\nDados do usuário inválidos. Certifique-se de que o nome é uma"
                                                        " string, o email é uma string e o cpf são apenas números.")
-        for usuario in self.__usuarios:
+        for usuario in self.__usuario_dao.get_all():
             if isinstance(usuario, UsuarioFisico) and (usuario.email == email or usuario.identificador() == cpf):
                 return self.__tela_usuario.mostra_mensagem("\nJá existe um usuário cadastrado com este e-mail ou CPF.")
         usuario_fisico = UsuarioFisico(nome, email, cpf)
-        self.__usuarios.append(usuario_fisico)
+        self.__usuario_dao.add(usuario_fisico.identificador(), usuario_fisico)
         self.__tela_usuario.mostra_mensagem("\nCadastro relizado com sucesso!")
 
     def abre_tela_usuario(self):
@@ -48,7 +49,7 @@ class ControladorUsuarios:
         if dados_edicao:
             self.usuario_logado.nome = dados_edicao["nome"]
             self.usuario_logado.email = dados_edicao["email"]
-            for usuario in self.__usuarios:
+            for usuario in self.__usuario_dao.get_all():
                 if usuario.identificador() == self.usuario_logado.identificador():
                     usuario.nome = dados_edicao["nome"]
                     usuario.email = dados_edicao["email"]
@@ -65,7 +66,7 @@ class ControladorUsuarios:
                 self.__tela_usuario.mostra_mensagem("\nOpção inválida. Tente novamente.")
 
     def deletar_usuario(self):
-        self.__usuarios.remove(self.usuario_logado)
+        self.__usuario_dao.remove(self.usuario_logado.identificador)
         self.usuario_logado = None
         self.__tela_usuario.mostra_mensagem("\nConta excluída com sucesso!")
         self.retornar()
@@ -76,7 +77,7 @@ class ControladorUsuarios:
     def efetuar_login(self):
         input_login_usuario = self.__tela_usuario.loga_usuario()
         if input_login_usuario:
-            for usuario in self.__usuarios:
+            for usuario in self.__usuario_dao.get_all():
                 if usuario.identificador() == input_login_usuario["id"] and \
                         usuario.email == input_login_usuario["email"]:
                     self.usuario_logado = usuario
