832aad846c585c5de71e9370561506685240a33b
Luiz Fernando Schelbauer <nandoschelba@gmail.com>
2023-06-22 22:27:13 -0300

    Add algumas telas

--- limite/tela_categoria.py
+++ limite/tela_categoria.py
@@ -1,28 +1,63 @@
+import PySimpleGUI as sg
+
+
 class TelaCategoria:
+    def __init__(self):
+        self.__window = None
 
     def tela_opcoes(self):
-        opcoes_validas = [0, 1, 2, 3, 4]
-        print("\n------ Opções de Categoria ------")
-        print("1 - Adicionar categoria")
-        print("2 - Listar categoria")
-        print("3 - Editar categoria")
-        print("4 - Excluir categoria")
-        print("0 - Voltar")
-        opcao = input("Escolha a opção desejada: ")
-        if not opcao.isdigit():
-            return
-        if int(opcao) not in opcoes_validas:
-            return
-        return int(opcao)
+        sg.ChangeLookAndFeel('Dark')
+        layout = [
+            [sg.Text('-------- Categorias ----------', font=("Helvica", 25))],
+            [sg.Radio('Adicionar categoria', "RD1", default=True, key='1')],
+            [sg.Radio('Listar categoria', "RD1", key='2')],
+            [sg.Radio('Editar categoria', "RD1", key='3')],
+            [sg.Radio('Excluir categoria', "RD1", key='4')],
+            [sg.Button('Confirmar'), sg.Cancel('Voltar')]
+        ]
+        self.__window = sg.Window('Categorias').Layout(layout)
+
+        button, values = self.__window.Read()
+        opcao = 0
+        if values.get('1'):
+            opcao = 1
+        if values.get('2'):
+            opcao = 2
+        if values.get('3'):
+            opcao = 3
+        if values.get('4'):
+            opcao = 4
+        if values.get('0') or button in (None, 'Voltar'):
+            opcao = 0
+        self.close()
+        return opcao
 
     def pega_dados_categoria(self):
-        nome = input("\nDigite o nome da categoria: ")
-        if not nome.strip():
-            return print("\nNome não pode estar vazio.")
-        descricao = input("Digite a descrição da categoria: ")
-        if not descricao.strip():
-            return print("\nDescrição não pode estar vazio.")
-        return {"nome": nome, "descricao": descricao}
+        sg.ChangeLookAndFeel('Dark')
+        layout = [
+            [sg.Text('-------- Nova categoria ----------', font=("Helvica", 25))],
+            [sg.Text('Nome:', size=(15, 1)), sg.InputText('', key='nome')],
+            [sg.Text('Descrição:', size=(15, 1)), sg.InputText('', key='descricao')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+        ]
+        self.__window = sg.Window('Nova categoria').Layout(layout)
+
+        button, values = self.open()
+        descricao = values['descricao']
+        nome = values['nome']
+
+        if button in (None, 'Cancelar'):
+            self.close()
+            return None
+        elif button == 'Confirmar':
+            if not nome.strip():
+                self.mostra_mensagem("Nome não pode estar vazio.")
+                return self.close()
+            if not descricao.strip():
+                self.mostra_mensagem("A descrição não pode estar vazia.")
+                return self.close()
+            self.close()
+            return {"nome": nome, "descricao": descricao}
 
     def mostra_categoria(self, dados_categoria):
         print("\n----------------------")
@@ -64,5 +99,12 @@ class TelaCategoria:
             return
         return int(opcao)
 
-    def mostra_mensagem(self, mensagem: str):
-        print(mensagem)
+    def mostra_mensagem(self, msg):
+        sg.popup("", msg)
+
+    def close(self):
+        self.__window.Close()
+
+    def open(self):
+        button, values = self.__window.Read()
+        return button, values
